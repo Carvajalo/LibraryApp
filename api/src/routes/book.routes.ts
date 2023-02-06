b3705f5a023a21d91a60bda2d8db94aa7b5d36ba
Orlando Meriño <carvajalo@uninorte.edu.co>
2023-02-06 10:11:49 -0500

    Book routes with admin role actualization and id validations

--- api/src/routes/book.routes.ts
+++ api/src/routes/book.routes.ts
@@ -2,9 +2,13 @@ import { Router } from "express";
 import * as bookController from "../controllers/book.controller";
 import { notSupported } from "../controllers/notSupported.controller";
 import { adminAuthMiddleware } from "../middlewares/auth.middleware";
+import { validateParamsId, validateBodyId } from "../utils/idValidation";
+
 const router = Router();
 
 router.get("/", bookController.getAllBooks);
+
+router.get("/:id", validateParamsId, bookController.getBook);
 
 router.post("/add", adminAuthMiddleware, bookController.addBook);
 
@@ -12,16 +16,40 @@ router.delete("/deleteAll", adminAuthMiddleware, bookController.deleteAllBooks);
 
 router.delete(
   "/delete/:id",
+  validateParamsId,
   adminAuthMiddleware,
   bookController.deleteBook
 );
 
 router.put("/update/:id", adminAuthMiddleware, bookController.updateBook);
 
+// Loan books routes:
+
+router.get("/recentLoans", bookController.getRecentLoans);
+
+router.post(
+  "/loan/:id",
+  validateParamsId,
+  validateBodyId,
+  adminAuthMiddleware,
+  bookController.loanBook
+);
+
+router.post(
+  "/return/:id",
+  validateParamsId,
+  adminAuthMiddleware,
+  bookController.returnBook
+);
+
+// Not supported routes:
+
 router.post("/add/:id", notSupported);
 
 router.delete("/delete", notSupported);
 
 router.put("/update", notSupported);
+
+router.post("/loan", notSupported);
 
 export default router;
