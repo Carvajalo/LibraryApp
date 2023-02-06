a1da04bdcb56b15ec6c1af8b8dde39f9462edb1b
Orlando Meriño <carvajalo@uninorte.edu.co>
2023-02-06 10:01:29 -0500

    User structured controller on different files (login and user CRUD)

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,15 +1,13 @@
 import { Request, Response } from "express";
 import User from "../models/user";
-import jwt from "jsonwebtoken";
-import config from "../config";
+import { validateToken } from "../services/jwt.service";
 
-const { JWT_SECRET } = config;
 export type userDataType = {
   id: string;
   role: string;
 };
 
-export const getAllUsers = async (req: Request, res: Response) => {
+export const getAllUsers = async (_: Request, res: Response) => {
   try {
     const users = await User.find();
     res.send(users);
@@ -18,78 +16,55 @@ export const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
-export const register = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res
-        .status(400)
-        .send({ message: "User with this email already exists" });
-    }
-    const newUser = new User({ name, email, password });
-    await newUser.save();
-    res.send({ message: "Successfully registered" });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-export const createAdmin = async (req: Request, res: Response) => {
-  const { name, email, password, role } = req.body;
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res
-        .status(400)
-        .send({ message: "Admin with this email already exists" });
-    }
-    const newUser = new User({ name, email, password, role });
-    await newUser.save();
-    res.send({ message: "Admin successfully registered" });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res
-        .status(400)
-        .send({ message: "Email or password is incorrect" });
-    }
-
-    const isMatch = await user.comparePassword(String(password));
-    if (!isMatch) {
-      return res
-        .status(400)
-        .send({ message: "Email or password is incorrect" });
-    }
-    const payload: userDataType = {
-      id: user._id,
-      role: user.role,
-    };
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
-
-    res.send({ token });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-export const logOut = async (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
   const token = req.headers["x-access-token"] as string;
   if (!token) {
     return res.status(401).send({ message: "No token provided" });
   }
   try {
-    const user = jwt.verify(token, JWT_SECRET);
-    res.send({ message: "Successfully logged out", user });
+    const user = validateToken(token);
+    res.send(user);
   } catch (error) {
     res.status(400).send({ message: "Invalid token" });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    return res.status(202).json(user);
+  } catch (error) {
+    res.status(400).send({ message: "Invalid token" });
+  }
+};
+
+export const updateUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updates = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(id, updates, { new: true });
+    res.status(200).send({ message: `User sucessfuly update`, user });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const deleteUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    res.status(200).send({ message: `User sucessfuly deleted`, user });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const deteteAllUsers = async (_: Request, res: Response) => {
+  try {
+    const user = await User.deleteMany();
+    res.status(200).send({ message: `All users sucessfuly deleted`, user });
+  } catch (error) {
+    res.status(400).send(error);
   }
 };
